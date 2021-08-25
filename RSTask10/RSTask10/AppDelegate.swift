3393a6733e6a02be1a74368d080e320445b3ddfa
Ivan Budovich <ibudovichivan3@gmail.com>
2021-08-25 10:34:07 +0300

    Setup Project

---
+++ RSTask10/RSTask10/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  RSTask10
+//
+//  Created by Ivan Budovich on 8/25/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        return true
+    }
+    
+}
---
+++ RSTask10/RSTask10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSTask10
+//
+//  Created by Ivan Budovich on 8/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
