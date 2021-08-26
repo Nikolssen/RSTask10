81c8792da204729b27f921c44fc5929caae26978
Ivan Budovich <ibudovichivan3@gmail.com>
2021-08-26 15:21:05 +0300

    Setup RollViewController

--- RSTask10/RSTask10/Controllers/RollViewController.swift
+++ RSTask10/RSTask10/Controllers/RollViewController.swift
@@ -6,13 +6,45 @@
 //
 
 import UIKit
+import AudioToolbox
 
 final class RollViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .clear
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dismissView)))
+        
+        let blurEffect = UIBlurEffect(style: .regular)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        
+        let imageView = UIImageView()
+        
+        
+        view.addSubview(imageView)
+        view.addSubview(visualEffectView)
+        
+        visualEffectView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            visualEffectView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            visualEffectView.topAnchor.constraint(equalTo: view.topAnchor),
+            visualEffectView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            visualEffectView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 120),
+            imageView.widthAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
     }
 
+    @objc func dismissView(){
+        self.dismiss(animated: true, completion: nil)
+    }
 }
