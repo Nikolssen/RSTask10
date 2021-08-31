aa4495f1f559b4d0767fc676fa7c479cd5ef3640
Ivan Budovich <ibudovichivan3@gmail.com>
2021-08-31 15:04:52 +0300

    Refactor Dashboard

--- RSTask10/RSTask10/Controllers/DashboardViewController.swift
+++ RSTask10/RSTask10/Controllers/DashboardViewController.swift
@@ -13,7 +13,7 @@ final class DashboardViewController: UIViewController {
     let tableView: UITableView = UITableView(frame: .zero, style: .plain)
     var tableViewHeightConstraint: NSLayoutConstraint!
     
-    let topLabel: UILabel = UILabel()
+    let headerLabel: UILabel = UILabel()
     var viewModel: DashboardViewModel!
     
     override func viewDidLoad() {
@@ -34,14 +34,14 @@ final class DashboardViewController: UIViewController {
         
         view.addSubview(startButton)
         view.addSubview(tableView)
-        view.addSubview(topLabel)
+        view.addSubview(headerLabel)
         
         tableView.backgroundColor = UIColor(named: "AppGrey")
         view.backgroundColor = UIColor(named: "AppBackground")
-        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.prefersLargeTitles = false
         navigationItem.largeTitleDisplayMode = .never
         navigationController?.navigationBar.isTranslucent = false
-
+        
         
         startButton.titleLabel?.font = UIFont(name: "Nunito-ExtraBold", size: 24)
         startButton.setTitle("Start game", for: .normal)
@@ -53,9 +53,9 @@ final class DashboardViewController: UIViewController {
         tableView.separatorColor = UIColor(red: 0.333, green: 0.333, blue: 0.333, alpha: 1)
         tableView.allowsSelection = false
         
-        topLabel.font = UIFont(name: "Nunito-ExtraBold", size: 36.0)
-        topLabel.text = "Game Counter"
-        topLabel.textColor =  UIColor.white
+        headerLabel.font = UIFont(name: "Nunito-ExtraBold", size: 36.0)
+        headerLabel.text = "Game Counter"
+        headerLabel.textColor =  UIColor.white
         if let rootViewController = UIApplication.shared.delegate?.window??.rootViewController, rootViewController !== navigationController {
             navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(dismissView))
         }
@@ -63,37 +63,38 @@ final class DashboardViewController: UIViewController {
     
     
     func configureLayout(){
-        topLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
         startButton.translatesAutoresizingMaskIntoConstraints = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableViewHeightConstraint =  tableView.heightAnchor.constraint(equalToConstant: 110)
         NSLayoutConstraint.activate([
             
-            startButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -65),
-            startButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            startButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Constants.startButtonBottomOffset),
+            startButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Constants.leadingOffset),
             startButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            startButton.heightAnchor.constraint(equalToConstant: 65),
-            tableView.topAnchor.constraint(equalTo: topLabel.bottomAnchor, constant: 25),
+            startButton.heightAnchor.constraint(equalToConstant: Constants.startButtonHeight),
+            tableView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: Constants.tableViewTopOffset),
             tableView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Constants.leadingOffset),
             tableViewHeightConstraint,
-            topLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            topLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            topLabel.heightAnchor.constraint(equalToConstant: 42.0)
+            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Constants.leadingOffset),
+            headerLabel.heightAnchor.constraint(equalToConstant: Constants.headerLabelHeight)
         ])
     }
     
     func configureViewModel(){
         viewModel.onPlayerAdd = {[unowned self] in
             self.tableView.reloadData()
-            let maximumHeight = view.safeAreaLayoutGuide.layoutFrame.height - 65 - 65 - 20 - 42 - 25
-            self.tableViewHeightConstraint.constant = min(maximumHeight, CGFloat(55 * (viewModel.players.count + 2)))
+            let maximumHeight = view.safeAreaLayoutGuide.layoutFrame.height - Constants.tableViewOffsetLimit
+            self.tableViewHeightConstraint.constant = min(maximumHeight, Constants.cellHeight * CGFloat(viewModel.players.count + 2))
         }
         
         viewModel.onPlayerDelete = {[unowned self] index in
+            self.tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .fade)
+            let maximumHeight = view.safeAreaLayoutGuide.layoutFrame.height - Constants.tableViewOffsetLimit
+            self.tableViewHeightConstraint.constant = min(maximumHeight, Constants.cellHeight * CGFloat(viewModel.players.count + 2))
             self.tableView.reloadData()
-            let maximumHeight = view.safeAreaLayoutGuide.layoutFrame.height - 65 - 65 - 20 - 42 - 25
-            self.tableViewHeightConstraint.constant = min(maximumHeight, CGFloat(55 * (viewModel.players.count + 2)))
         }
     }
     
@@ -118,11 +119,11 @@ extension DashboardViewController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: UserCell.id) as! UserCell
+        
         if indexPath.row == viewModel.players.count{
             cell.style = .add
             cell.handler = {[unowned self] in
                 self.viewModel.addPlayer()
-                
             }
         }
         
@@ -133,19 +134,17 @@ extension DashboardViewController: UITableViewDelegate, UITableViewDataSource{
             cell.handler = { [unowned self, indexPath] in
                 self.viewModel.deletePlayer(at: indexPath.row)
             }
-            
-            
         }
         cell.backgroundColor = UIColor(named: "AppGrey")
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 55.0
+        Constants.cellHeight
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 55.0))
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: Constants.cellHeight))
         view.backgroundColor = UIColor(named: "AppGrey")
         
         let label = UILabel(frame: .zero)
@@ -163,11 +162,24 @@ extension DashboardViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 55
+        Constants.cellHeight
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        1
     }
     
+}
+
+extension DashboardViewController{
+    
+    enum Constants{
+        static let cellHeight: CGFloat = 55.0
+        static let startButtonHeight: CGFloat = 65.0
+        static let startButtonBottomOffset: CGFloat = 65.0
+        static let headerLabelHeight: CGFloat = 42.0
+        static let tableViewTopOffset: CGFloat = 25.0
+        static let tableViewOffsetLimit: CGFloat = headerLabelHeight + startButtonHeight + startButtonBottomOffset + tableViewTopOffset + 20.0
+        static let leadingOffset: CGFloat = 20.0
+    }
 }
